329d164f14dbc3c35b9e77b6bc8117377fcb9717
Thaís Silva <thaisdss.silva@gmail.com>
2023-01-27 06:17:01 -0300

    Update index.tsx

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -4,7 +4,7 @@ export function Projects() {
     return(
         <C.Container>
             <h2>Meus Projetos</h2>
-            <p><a href="https://github.com/thaisdss?tab=repositories">Veja Todos</a></p>
+            <p><a href="https://github.com/thaisdss?tab=repositories" target="_blank">Veja Todos</a></p>
         </C.Container>
     );
 }
