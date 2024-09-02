1b297643c2f18c1096d3a2213ff0499f59698c64
Thaís Silva <91297976+thaisdss@users.noreply.github.com>
2024-09-02 18:03:19 -0300

    Update index.tsx

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -10,9 +10,9 @@ export function AboutMe( {width}: Props ) {
             <h2>Sobre mim</h2>
             <C.AboutMeContainer>
                 <p>No início de 2021, comecei a estudar Excel avançado e, então, despertei interesse pela área de TI. Foi quando comecei a pesquisar sobre e me apaixonei por programação.</p>
-                <p>Atualmente estou me especializando na área de Front-End.</p>
-                <p>Amo estudar e tenho facilidade em me adaptar e aprender novas tecnologias.</p>
-                <p>Estou a procura da primeira oportunidade na área!</p>
+                <p>Atualmente, estudo e trabalho com NextJS, ReactJS, Typescript, Styled-Components e outras tecnologias web.</p>
+                <p>Meu objetivo é me especializar em desenvolvimento front-end</p>
+                <p>No futuro, quero aprender o básico de desenvolvimento back-end, especialmente Node.js. Também desejo trabalhar com desenvolvimento mobile.</p>
             </C.AboutMeContainer>
         </C.Container>
     );
