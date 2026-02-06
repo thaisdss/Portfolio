57e3d230157444df25128a0bd95e7b51d5b9d555
Thaís Silva <thaisdss.silva@gmail.com>
2026-02-06 19:12:03 -0300

    Updated styles

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export function App() {
     const user = useGithub("thaisdss", ["banestes-test", "Mini-Projects-React", "Mini-Projects-JS"])
 
     return (
-        <div className="w-full h-full bg-slate-800 font-sans px-8 py-4 flex flex-col gap-12 max-lg:gap-8">
+        <div className="w-full h-full bg-slate-800 font-sans px-8 py-4 flex flex-col gap-8 max-lg:gap-8">
             <div className="flex items-start gap-8 rounded-xl p-8 text-slate-200 bg-slate-500/5 border border-slate-700 max-lg:flex-col">
                 <img src={user.avatarUrl} alt="Avatar" className="w-32 h-32 rounded-full mb-4 border-2 border-emerald-500 max-lg:self-center" />
 
@@ -18,8 +18,8 @@ export function App() {
                     <p className="text-emerald-500 text-sm">Front-end Developer</p>
 
                     <p className="text-sm mt-5">Iniciei minha trajetória em TI após um curso de JavaScript e, desde então, atuo no desenvolvimento front-end para aplicações web e mobile.</p>
-                    <p className="text-sm">Atualmente, atuo no desenvolvimento de sistemas voltados ao suporte das demandas da área de crédito rural em bancos e cooperativas financeiras.</p>
-                    <p className="text-sm">Acredito que aprender continuamente e adaptar-se a novas tecnologias é essencial para criar boas soluções.</p>
+                    <p className="text-sm max-lg:mt-1">Atualmente, atuo no desenvolvimento de sistemas voltados ao suporte das demandas da área de crédito rural em bancos e cooperativas financeiras.</p>
+                    <p className="text-sm max-lg:mt-1">Acredito que aprender continuamente e adaptar-se a novas tecnologias é essencial para criar boas soluções.</p>
                 </div>
             </div>
 
