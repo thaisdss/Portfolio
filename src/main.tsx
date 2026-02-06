89822080e2518807888601dfab06a12b9cee25ad
Thaís Silva <thaisdss.silva@gmail.com>
2026-02-06 19:04:21 -0300

    remove react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
-import { RouterProvider } from 'react-router'
-import { router } from './routes/index.tsx'
 import "./global.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <RouterProvider router={router} />
   </StrictMode>,
 )
